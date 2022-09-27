51963d6c3d20f6e5c83efde532ee1134450e2bb1
Cristóbal Latorre Padilla <clatorre@bcnschool.cl>
2022-09-27 16:52:24 -0300

    Create folders and files for lang strings

---
+++ lang/en/block_latorre.php
@@ -0,0 +1,28 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_latorre', language 'en'
+ *
+ * @package   block_latorre
+ * @copyright 2022 Cristóbal Latorre Padilla - clatorre@bcnschool.cl
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Latorre block';
+$string['simplehtml'] = 'Latorre';
+$string['simplehtml:addinstance'] = 'Add a new latorre block';
+$string['simplehtml:myaddinstance'] = 'Add a new latorre block to the My Moodle page';
